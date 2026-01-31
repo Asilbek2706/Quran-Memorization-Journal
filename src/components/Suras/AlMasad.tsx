8e218b25d0a2094f4a998ddc784785f52deaaa24
Asilbek2706 <249135167+Asilbek2706@users.noreply.github.com>
2026-01-31 18:25:18 +0500

    Masad surah

---
+++ src/components/Suras/AlMasad.tsx
@@ -0,0 +1,36 @@
+import SuraCard from "../SuraCard.tsx";
+
+const AlMasad = () => {
+    const original = (
+        <>
+            Bismillahir Rohmanir Rohiym<br/>
+            Tabbat yadaa abii lahabiv-va tabb<br/>
+            Maa ag'naa 'anhu maaluhu va maa kasab<br/>
+            Sayaslaa naaron zaata lahab<br/>
+            Vamroatuhu hammaalatal hatob<br/>
+            Fii jiidihaa hablum-mim masad
+        </>
+    );
+
+    const translation = (
+        <>
+            Mehribon va Rahmli Alloh nomi bilan<br/>
+            Abu Lahabning qo'llari halok bo'lsin, halok!<br/>
+            Mol-mulki va to'plagan boyliklari unga foyda bermadi.<br/>
+            Yaqinda u alangalangan olovga kiradi.<br/>
+            Uning o'tin tashuvchi xotini ham (olovga kiradi).<br/>
+            Uning bo'ynida pishiq eshilgan ip (arvoh) bo'ladi.<br/>
+        </>
+    );
+
+    return (
+        <SuraCard title={"113. Al-Masad surasi"}
+                  audioURL={"src/suras-audio/Masad.mp3"}
+                  originalText={original}
+                  translationText={translation}
+        >
+        </SuraCard>
+    );
+}
+
+export default AlMasad;
