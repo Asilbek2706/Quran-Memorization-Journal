02fed3968d9f49c6772887752afb322910e759a9
Asilbek2706 <249135167+Asilbek2706@users.noreply.github.com>
2026-01-31 18:25:40 +0500

    Ihlas surah

---
+++ src/components/Suras/Ixlos.tsx
@@ -0,0 +1,34 @@
+import SuraCard from "../SuraCard.tsx";
+
+const Ixlos = () => {
+    const original = (
+        <>
+            Bismillahir Rohmanir Rohiym<br/>
+            Qul huvallohu ahad<br/>
+            Allohus-somad<br/>
+            Lam yalid va lam yulad<br/>
+            Va lam yakullahu kufuvan ahad<br/>
+        </>
+    );
+
+    const translation = (
+        <>
+            Mehribon va Rahmli Alloh nomi bilan<br/>
+            (Ey Muhammad), ayting: «U Alloh yagonadir».<br/>
+            «Alloh Somaddir (barcha hojatlar bilan ko'zlanadigan, hech kimga muhtoj bo'lmagan, hamma unga muhtoj zotdir)».<br/>
+            «U tug'magan va tug'ilmagan».<br/>
+            «Va Unga hech kim teng bo'lmagan».<br/>
+        </>
+    );
+
+    return (
+        <SuraCard title={"112. Ixlos surasi"}
+                  audioURL={"src/suras-audio/Ixlos.mp3"}
+                  originalText={original}
+                  translationText={translation}
+        >
+        </SuraCard>
+    );
+}
+
+export default Ixlos;
